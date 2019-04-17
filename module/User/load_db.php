1e2f73296c88493af6609757b4cd5c28acc2a932
Guru2921 <guru.softwaremaster@mail.ru>
2019-04-17 11:24:10 +1000

    deleted files

--- module/User/load_db.php
+++
@@ -1,7 +0,0 @@
-<?php
-$db = new PDO('sqlite:' . realpath(__DIR__) . '/zfchat.db');
-$fh = fopen(__DIR__ . '/crud.sql', 'r');
-while ($line = fread($fh, 4096)) {
-    $db->exec($line);
-}
-fclose($fh);
